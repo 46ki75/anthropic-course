63cccf3bc8d66df26eca10a35a705069e014a53d
Ikuma Yamashita <me@ikuma.cloud>
2026-05-13 20:06:40 +0900

    feat: Implement request to Anthropic API for quantum computing query

---
+++ src/making-request.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-haiku-4-5"
+
+message = client.messages.create(
+    model=model,
+    max_tokens=1000,
+    messages=[
+        {"role": "user", "content": "What is quantum computing? Answer in one sentence"}
+    ],
+)
+
+for content in message.content:
+    print(content.text)
